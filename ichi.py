6252306719abd4a6dfa42dc69fea9ab86db7ce5d
Nusrat-Suzana <nusrat.suzana.bup20@gmail.com>
2024-10-10 14:08:51 +0600

    Create ichi.py

---
+++ ichi.py
@@ -0,0 +1,38 @@
+# hello world
+
+print("Hello World")
+
+#  arithmatical operations
+
+# addition
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+sum_result = num1 + num2
+print(f"sum: {num1} + {num2} = {sum_result}")
+
+# subtraction
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+diff = num1 - num2
+print(f"difference: {num1} - {num2} = {diff}")
+
+# multiplication
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+product = num1 * num2
+print(f"product: {num1} * {num2} = {product}")
+
+
+# division
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+if num2 == 0:
+  print("Error: Divide by zero is not allowed.")
+else:
+  div_result = num1 / num2
+print(f"quotient: {num1} / {num2} = {div_result}")
+
+
+
+
+
